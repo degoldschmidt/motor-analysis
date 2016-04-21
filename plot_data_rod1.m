88c2811ea45e98d67e985b0a62f288a5c443e266
Rodarkh <rodrigo.d.1990@gmail.com>
2016-04-21 18:43:49 +0100

    help section improved - plot individual traces of error

--- plot_data_rod1.m
+++ plot_data_rod1.m
@@ -1,7 +1,21 @@
-function [] = plot_data_rod1( all_analysis )
-%PLOT_DATA_ROD1 - have the analysis file in your workspace and run it to
-%plot cool stuff
-%   Detailed explanation goes here
+function [] = plot_data_rod1( all_analysis,plot_individuals,save_fig )
+%PLOT_DATA_ROD1 
+% plot_data_rod1( all_analysis,plot_individuals,save_fig ) - function
+% definition
+%
+% all_analysis = analysis; %file in your workspace
+% plot_individuals = 0 or 1; %plot a single figure per indivdual -carefull,
+% this opens A LOT of new figures
+% save_fig = 0 or 1; % to save figures. Not yet implemented.
+%
+% Have the ANALYSIS file in your workspace and simply run
+%the function like in example 1.
+% If you would like to plot all analysis at the same time, create a cell
+% with the analysis file inside - see example 2.
+% EXAMPLES:
+%  1-> plot_data_rod1( {analysis},0,1 ) - example
+%  2-> plot_data_rod1( {analysis_v1, analysis_v2, analysis_v3},1,1 ) - example
+%
 
 for file=1:length(all_analysis)
     
@@ -9,7 +23,7 @@ for file=1:length(all_analysis)
     n_subjects = length(analysis);
     block={'Train','Test','After'};
     for i=1:3
-        angleError.(block{i})=zeros(length(analysis{1,1}.(block{i}).ErrAngss),n_subjects);
+        angleError.(block{i})=[];
     end
     for j=1:3
         for i=1:n_subjects
@@ -20,13 +34,28 @@ for file=1:length(all_analysis)
     figure
     for j=1:3
         h(j)=subplot(3,1,j);
+        hold on
         plot(smooth(mean(angleError.(block{j}),2)))
+        plot([0 length(mean(angleError.(block{j}),2))],[0 0],'--k','LineWidth',0.5)
         ylabel(block{j})
         xlabel('Trials')
-        ylim([-20 20])
+        ylim([-50 50])
     end
     title(h(1),[analysis{1,1}.task_version, ' - n=' ,num2str(n_subjects)]);
     
+    if plot_individuals
+    for j=1:3
+        for i=1:n_subjects
+            figure(file*100 +20*j + i-1)
+            hold on
+            plot(angleError.(block{j})(:,i))
+            plot([0 length(mean(angleError.(block{j}),2))],[0 0],'--k','LineWidth',0.5)
+            title([analysis{1,i}.task_version, '-' block{j}, '-', analysis{1,i}.name])
+            ylabel('AngleError(º)')
+            xlabel('Trials')
+        end
+    end
+    end
 end
 end
 
