408c733dcef9380ff670545a6b81a2b919fb3fe3
degoldschmidt <goldschmidt.de@gmail.com>
2016-04-22 09:15:19 +0100

    Individual trajectories
    YES!

--- polar_traj.m
+++ polar_traj.m
@@ -30,14 +30,15 @@ color(:,6) = [ 0.     1.      .667 ];   %% CYAN
 color(:,7) = [ 0.     0.333  1.    ];   %% BLUE
 color(:,8) = [ 0.667  0.     1.    ];   %% VIOLET
 block={'Train','Test','After'};
+rcut = 520;
 
 for file=1:length(all_analysis)
     analysis=all_analysis{1};
     n_subjs = length(analysis);
     
     for subj=1:1
-        for bl=2:2
-            %subplot(1,3,bl);
+        for bl=1:length(block)
+            subplot(1,3,bl);
             polar(0,500,'-k');
             hold on;
             first = analysis{1,subj}.(block{bl}).TrajsTang;
@@ -45,14 +46,17 @@ for file=1:length(all_analysis)
                 for i=1:length(first)
                     [dir i]
                     traj = cell2mat(first(i,dir));
-                    if length(traj)>0
+                    if (length(traj)>0 && length(traj)<100)
                         x= traj(:,1);
                         y= traj(:,2);
                         theta = atan2(y,x);
                         rho = sqrt(x.*x+y.*y);
-                        h = polar(theta, rho);
-                        %rlim= 500;
-                        %axis([-1 1 -1 1]*rlim);
+                        
+                        h = polar(theta(rho<rcut), rho(rho<rcut));
+                        hax = findall(gca,'type','text');
+                        legit = {'0','30','60','90','120','150','180','210','240','270','300','330',''};
+                        idx   = ~ismember(get(hax,'string'),legit);
+                        set(hax(idx),'string','')
                         hold on;
                         set(h, 'Color', color(:,dir));
                     end
